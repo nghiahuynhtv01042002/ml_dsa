d4e56d862c1000f3ce35465aae3604735972461d
nghiahuynhtv01042002 <nghiahuynhtv01042002@gmail.com>
2026-06-27 16:40:49 +0700

    [update] add module

---
+++ src/math/mod_q.c
@@ -0,0 +1,39 @@
+/*
+ * @file mod_q.c
+ * @brief 
+ * @author Nghia Huynh AKA Txn
+ * @date 2026-06-27
+ */
+#include "modq.h"
+
+u32 modq_add(u32 a, u32 b, u32 q)
+{
+    u32 ret_val = 0;
+    ret_val  =(u32)(a + b);
+    if (ret_val >= q)
+    {
+        ret_val  -=q;
+    }
+    return ret_val;
+}
+static u32 sub_mod (u32 a, u32 b, u32 q)
+{
+    u32 ret_val = 0;
+    if (a >= b)
+    {
+        ret_val = (u32)(a - b);
+    }
+    else 
+    {
+        ret_val = (u32)(a + q) - b;
+    }
+    return ret_val;
+}
+
+static u32 mul_mod(u32 a, u32 b, u32 q)
+{
+    u64 ret_val = 0;
+    ret_val = (u64) a * (u64) b ;
+    ret_val %= (u64)q;
+    return (u32)ret_val;
+}
---
+++ src/math/mod_q.h
@@ -0,0 +1,19 @@
+/*
+ * @file mod_q.h
+ * @brief 
+ * @author Nghia Huynh AKA Txn
+ * @date 2026-06-27
+ */
+
+#ifndef MOD_Q_H
+#define MOD_Q_H
+#include "params.h"
+#include "types_def.h"
+#define MOD_Q (7)
+#define MOD_N (256)
+
+static u32 modq_add(u32 a, u32 b, u32 q);
+static u32 modq_sub(u32 a, u32 b, u32 q);
+static u32 modq_mul(u32 a, u32 b, u32 q);
+
+#endif  // MOD_Q_H
